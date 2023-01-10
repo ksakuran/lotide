66eb59c3d25ded25ac49d9e085e7a38099ebf124
Kat Nishimura <katherine.nishimura@live.ca>
2023-01-10 01:30:49 +0000

    a function the returns the first element of an array

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`💸️💸️💸️Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    return console.log(`🚨️🚨️🚨️Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  return array[0];
+};
+
+// TEST CODE
+assertEqual(head([5,6,7]),5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]),undefined);
