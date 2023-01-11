ca7d6e1ab4a134d1a6ae25ec6fa9c21e81981481
Kat Nishimura <katherine.nishimura@live.ca>
2023-01-11 21:01:15 +0000

    adding edge case for empty array with error message, linted

--- without.js
+++ without.js
@@ -25,13 +25,16 @@ const assertArraysEqual = function(array1, array2) {
 
 const without = function(source,itemsToRemove) {
   let finalArray = [];
+  if (source.length === 0 || itemsToRemove.length === 0) {
+    return console.log("Error: Do not use an empty array");
+  }
   for (let i = 0; i < source.length; i++) {
     if (!itemsToRemove.includes(source[i])) {
-      finalArray.push(source[i]);      
-    } 
+      finalArray.push(source[i]);
+    }
   }
-  return console.log(finalArray);
-}
+  return finalArray;
+};
 
 //inputs are two arrays
 // source array must remain unmodified
@@ -40,11 +43,12 @@ const without = function(source,itemsToRemove) {
 //must return a new array
 
 //TEST CODE;
-without([1, 2, 3], [1]); // => [2, 3]
-without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+//without([1, 2, 3], [1]); // => [2, 3]
+//without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+//console.log(without ([],[1,2]));
 
 //TEST CODE make sure original array is not modified
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
+//const words = ["hello", "world", "lighthouse"];
+//without(words, ["lighthouse"]); // no need to capture return value for this test case
 //Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+//assertArraysEqual(words, ["hello", "world", "lighthouse"]);
