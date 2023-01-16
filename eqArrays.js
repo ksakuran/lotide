86809fd9a13ea926df0318335e473028fa0d50f5
Kat Nishimura <katherine.nishimura@live.ca>
2023-01-16 23:07:49 +0000

    created a test file for eqArrays, removed the testing from the actual eqArrays file

--- eqArrays.js
+++ eqArrays.js
@@ -13,13 +13,6 @@ const eqArrays = function(arr1, arr2) {
   } return true;
 };
 
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false); // => should PASS
 
-// //console.log(eqArrays([1,2,3], [1,2,4]))
-
-// assertEqual(eqArrays([1,2,3],["a", "b", "c"]), true);  // should fail
-// assertEqual(eqArrays([1,2,3],[1,2,3]), true); //true  pass
-// assertEqual(eqArrays([1,2,3],[4,5,6,7,8]), false); //false pass
 
 module.exports = eqArrays
