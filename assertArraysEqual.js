18f1cef7294ba79c520278b9451e9048c3de7c5a
Kat Nishimura <katherine.nishimura@live.ca>
2023-01-17 00:36:25 +0000

    created new test file for assertArraysEqual, module exported

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,9 +4,8 @@ const eqArrays = require('./eqArrays')
 const assertArraysEqual = function (array1, array2) {
   if (eqArrays(array1, array2) === true) {
     return console.log(`💸️💸️💸️Assertion Passed: ${array1} === ${array2}`);
-  } else if (eqArrays(array1, array2) === false) {
-    return console.log(`🚨️🚨️🚨️Assertion Failed: ${array1} !== ${array2}`);
-  }
+  } 
+  return console.log(`🚨️🚨️🚨️Assertion Failed: ${array1} !== ${array2}`);
 };
 
 // assertArraysEqual([1,2,3],[1,2,3]);
