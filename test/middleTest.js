29b389426187b09aefa099297364a8bc15189581
Kat Nishimura <katherine.nishimura@live.ca>
2023-01-17 02:20:38 +0000

    rewriting test for middle with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -21,5 +21,9 @@ describe("#middle", () => {
   it("returns [2] for [1, 2, 3]", () => {
     assert.deepEqual(middle([1, 2, 3]), [2]);
   }); 
+
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  }); 
   
 });
